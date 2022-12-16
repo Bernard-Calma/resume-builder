74b6a24c56f112955d8febd5ec5993a61d23cc7c
Bernard-Calma <nard.calma@gmail.com>
2022-12-16 02:28:09 -0800

    added header container and navigation links

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,9 +1,22 @@
 const Header = () =>{
+
     return(
-        <div>
-            Header
+        <div style={Styles.container}>
+            <h3>=</h3>
+            <h1>Resume Builder</h1>
+            <h3>Login</h3>
         </div>
     )
 }
 
 export default Header
+
+
+const Styles = {
+    container: {
+        display: "flex",
+        justifyContent: "space-between",
+        marginLeft: 20,
+        marginRight: 20
+    }
+}
